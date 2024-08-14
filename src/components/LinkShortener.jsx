c9479eb4770b3298cfb70437537b3261058c4d32
cassia <cassiasadler96@gmail.com>
2024-08-14 10:41:29 +0100

    Add new vite environment variables

--- src/components/LinkShortener.jsx
+++ src/components/LinkShortener.jsx
@@ -8,18 +8,35 @@ export default function LinkShortener() {
     const [copied, setCopied] = useState(false);
 
     const handleSubmit = async () => {
-        const token = process.env.REACT_APP_BITLY_API_KEY;
-        const res = await fetch('https://api-ssl.bitly.com/v4/shorten', {
-            method: 'POST',
-            headers: {
-                'Authorization': `Bearer ${token}`,
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ "long_url": `${linkInput}`, "domain": "bit.ly" })
-        })
-        const result = await res.json();
-        setShortLink(result.link);
-    }
+        try {
+            const token = import.meta.env.VITE_BITLY_API_KEY;
+            console.log('API Token:', token);
+
+            if (!token) {
+                throw new Error('API key is missing.');
+            }
+
+            const res = await fetch('https://api-ssl.bitly.com/v4/shorten', {
+                method: 'POST',
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ "long_url": `${linkInput}`, "domain": "bit.ly" })
+            });
+
+            if (!res.ok) {
+                console.error('Error:', res.statusText);
+                return;
+            }
+
+            const result = await res.json();
+            setShortLink(result.link);
+
+        } catch (error) {
+            console.error('Error:', error.message);
+        }
+    };
 
     return (
         <div className="link-shortener-wrapper">
